d2cbc42a37d8cd0960214c0db1b26dc517736b9a
Jared babinec <jbabinec1@gmail.com>
2017-04-18 15:54:57 -0400

    Update index.js

--- todov2 (1)/js/index.js
+++ todov2 (1)/js/index.js
@@ -1,55 +1,56 @@
-$(function () {
-  function addItem () {
-    // append to the list
-    $(".tasks").append('<li><input type= "checkbox" class="toggle" /><span>' + $("#todo-input").val() + '</span> <small><a href="#edit">Edit</a> &bull; <a href="#delete">Delete</a></small></li>');
-    
-    // clear the text
-    $("#todo-input").val("");
-  }
-   
+function addItem () {
+
+//get class
+var priority_class = $('.choose-priorety')
+.clone()
+.removeClass('choose-priorety')
+.attr('class');
+
+// append to the list
+$(".tasks").append('<li class="' + priority_class + '"><input type= "checkbox" class="toggle" /><span>' + $("#todo-input").val() + '</span> <small><a href="#edit">Edit</a> &bull; <a href="#delete">Delete</a></small></li>');        
+
+// clear the text
+$("#todo-input").val("");
+
+}
+
+
    ///// Check all boxes 
-  
+
   $("#toggle-all").click(function () {
      $('input:checkbox').not(this).prop('checked', this.checked);
  });
-  
- 
-  /// Delete all checked boxes 
-  
-  $(document).on("click", '#clearCompleted', function() {
-  $(".toggle:checked").each(function () {
-    $(this).closest("li").remove();
-  });
-});  
-  
-  
-   
+
+
    //strike-through text when box checked, unstrike when unchecked. 
-  
-  
+
+
   $(document).on("click", '.toggle', function() {
     if ($(this).closest("li").find("span").css('textDecoration') === 'line-through') {
           $(this).closest("li").find("span").css('textDecoration', 'none');
     } else {
       $(this).closest("li").find("span").toggleClass('stroked');
-      
+
     }
   }); 
-  
-   
-   // Submit with when enter key is pressed, and clicking add button
-   
+
+
+/// add task when enter key is pressed
+
   $("#todo").keydown(function (e) {
-   
-    if (e.which == 14)
+    
+    if (e.which == 13)
       addItem();
   });
-        
-  $('#todo-form').on('submit',function(e) {
+
+
+  // on clicking the add button
+$('#add-task').on('click',function(e) {
     e.preventDefault();
     addItem();
-  })
-  
+})
+
+
   // delegate the events to dynamically generated elements
   // for the edit button
   $(document).on("click", 'a[href="#edit"]', function () {
@@ -58,7 +59,51 @@ $(function () {
     $(this).closest("li").find("span").prop("contenteditable", true).focus();
     return false;
   });
-  
+
+
+  /// Delete all checked boxes 
+
+  $(document).on("click", '#clearCompleted', function() {
+  $(".toggle:checked").each(function () {
+    $(this).closest("li").remove();
+  });
+});  
+
+
+
+  $(document).ready(function() {
+
+  var $addForm = $('#todo-form');
+  var $taskInput = $addForm.find('#todo-input');
+  var $todoList = $('.tasks-parent li');
+  var fadeSpeed = 300;
+
+  })
+
+
+
+        //priorities btn display list of urgency with 3 buttons
+
+  var $prioritiesContainer = $('.priorities');
+  var $prioritiesList = $prioritiesContainer.find('.priorities-list');
+  var $choosePriorityBtn = $prioritiesContainer.find('.choose-priorety');
+  var $prioritiesBtns = $prioritiesContainer.find('.priority button');
+
+  $choosePriorityBtn.on('click',function() {
+    $prioritiesList.toggle();
+  });
+  $prioritiesBtns.on('click',function() {
+    $choosePriorityBtn.removeClass('low medium high').addClass($(this).attr('class'));
+    $prioritiesList.hide();
+  });
+
+
+
+
+
+
+
+
   // for the delete button
   $(document).on("click", 'a[href="#delete"]', function () {
     // remove the list item
@@ -67,4 +112,3 @@ $(function () {
     });
     return false;
   });
- });
