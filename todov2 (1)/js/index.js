e6b74f549fe29b433073da10203af533483657bb
Jared babinec <jbabinec1@gmail.com>
2017-05-05 15:15:28 -0400

    Update index.js
    Cleaned this up a bit, the structure was so bad that it crashed almost after every other time after running once. Moved the 'priority' button out of the form div to avoid crashing.

--- todov2 (1)/js/index.js
+++ todov2 (1)/js/index.js
@@ -1,71 +1,4 @@
-function addItem () {
-var priority_class = $('.choose-priorety')
-.clone()
-.removeClass('choose-priorety')
-.attr('class');
-
-// append to the list
-$(".tasks").append('<li class="' + priority_class + '"><input type= "checkbox" class="toggle" /><span>' + $("#todo-input").val() + '</span> <small><a href="#edit">Edit</a> &bull; <a href="#delete">Delete</a></small></li>');        
-
-// clear the text
-$("#todo-input").val("");
-
-}
-
-
- ///// Check all boxes 
-  $("#toggle-all").click(function () {
-  $('input:checkbox').not(this).prop('checked', this.checked);
- });
-
-
-
-   //strike-through text when box checked, unstrike when unchecked. 
-  $(document).on("click", '.toggle', function() {
-    if ($(this).closest("li").find("span").css('textDecoration') === 'line-through') {
-        $(this).closest("li").find("span").css('textDecoration', 'none');
-        } else {
-      $(this).closest("li").find("span").toggleClass('stroked');
-
-        }
-        }); 
-
-
-
-     /// add task when enter key is pressed
-      $("#todo").keydown(function (e) {
-       if (e.which == 13)
-       addItem();
-    });
-
-
-    // on clicking the add button
-      $('#add-task').on('click',function(e) {
-      e.preventDefault();
-      addItem();
-})
-
-
-     // delegate the events to dynamically generated elements
-    // for the edit button
-      $(document).on("click", 'a[href="#edit"]', function () {
-    
-    // make the span editable and focus it
-      $(this).closest("li").find("span").prop("contenteditable", true).focus();
-      return false;
-      });
-
-
-  /// Delete all checked boxes 
-      $(document).on("click", '#clearCompleted', function() {
-      $(".toggle:checked").each(function () {
-      $(this).closest("li").remove();
-      });
-     });  
-
-
-
-     $(document).ready(function() {
+$(document).ready(function() {
   var $addForm = $('#todo-form');
   var $taskInput = $addForm.find('#todo-input');
   var $todoList = $('.tasks-parent li');
@@ -74,28 +7,90 @@ $("#todo-input").val("");
   })
 
 
-
-        /// Priorities btn display list of urgency with 3 buttons
+//Priorities btn display list of urgency with 3 buttons
   var $prioritiesContainer = $('.priorities');
   var $prioritiesList = $prioritiesContainer.find('.priorities-list');
   var $choosePriorityBtn = $prioritiesContainer.find('.choose-priorety');
   var $prioritiesBtns = $prioritiesContainer.find('.priority button');
 
-  $choosePriorityBtn.on('click',function() {
-  $prioritiesList.toggle();
-     });
-  $prioritiesBtns.on('click',function() {
-  $choosePriorityBtn.removeClass('low medium high').addClass($(this).attr('class'));
-  $prioritiesList.hide();
+
+ $choosePriorityBtn.on('click',function() {
+ $prioritiesList.toggle({direction:"left"});
+  });
+ $prioritiesBtns.on('click',function() {
+ $choosePriorityBtn.removeClass('low medium high').addClass($(this).attr('class'));
+ $prioritiesList.hide();
   });
 
+
+
+/// Add task when enter key is pressed
+ $("#todo").keydown(function (e) {
+    if (e.which == 13)
+     addItem();
+  });
+
+  // On clicking the add button
+ $('#add-task').on('click',function() {
+    addItem();
+  })
+
+ function addItem () { 
+  //get class
+  var priority_class = $('.choose-priorety')
+   .clone()
+   .removeClass('choose-priorety')
+   .attr('class');
+
+   // append to the list
+ $(".tasks").append('<li class="' + priority_class + '"><input type= "checkbox" class="toggle" /><span>' + $("#todo-input").val() + '</span> <small><a href="#edit">Edit</a> &bull; <a href="#delete">Delete</a></small></li>');        
+}
+  // Delegate the events to dynamically generated elements
+  // For the edit button
+ $(document).on("click", 'a[href="#edit"]', function () {               
+  // Make the span editable  
+ $(this).closest("li").find("span").prop("contenteditable", true).focus();
+    
+   return false;
+  });
+
+
+// Clear the text
+ $("#todo-input").val("");
+
+
+   ///// Check all boxes 
+ $("#toggle-all").click(function () {
+ $('input:checkbox').not(this).prop('checked', this.checked);
+ });
+
+
+   //strike-through text when box checked, unstrike when unchecked. 
+
+var underline =  $('.underline');
+  $(document).on("click", '.toggle', function() {
+    if ($(this).closest("li").find("span").css('textDecoration') === 'line-through') {
+          $(this).closest("li").find ($underline).toggle({direction:"right"})
+    } else {
+      $(this).closest("li").find("span").toggleClass('stroked');
+
+    }
+  }); 
+
+
+  /// Delete all checked boxes 
+  $(document).on("click", '#clearCompleted', function() {
+  $(".toggle:checked").each(function () {
+  $(this).closest("li").remove();
+  });
+});  
 
 
   // for the delete button
   $(document).on("click", 'a[href="#delete"]', function () {
     // remove the list item
-   $(this).closest("li").fadeOut(function () {
-   $(this).remove();
+  $(this).closest("li").fadeOut(function () {
+  $(this).remove();
     });
     return false;
   });
